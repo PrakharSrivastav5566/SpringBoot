9d3578c1f6b07c0f5a6d1e318aac02a321dd8fc5
PrakharSrivastav5566 <prakhar.srivastava_cs23@gla.ac.in>
2026-01-19 15:20:48 +0530

    added first APIs

--- Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
+++ Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
@@ -4,11 +4,23 @@ import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent() {
-        StudentModel student = new StudentModel(1, "Prakhar", "ps@gmail.com");
-        return student;
+//    public StudentModel getStudent() {
+//        StudentModel student = new StudentModel(1, "Prakhar", "ps@gmail.com");
+//        return student;
+//    }
+
+    public ArrayList<StudentModel> getStudent() {
+
+        ArrayList<StudentModel> al = new ArrayList<>();
+        al.add(new StudentModel(1, "Prakhar", "ps@gmail.com"));
+        al.add(new StudentModel(2, "Shukla", "as@gmail.com"));
+        al.add(new StudentModel(3, "Moksh", "mu@gmail.com"));
+
+        return al;
     }
 }
