9f6a5d62b48295ac72d1d7e6ea7a11c15db7dbad
PrakharSrivastav5566 <prakhar.srivastava_cs23@gla.ac.in>
2026-01-19 15:04:14 +0530

    called model_obj

--- Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
+++ Day2Part2/src/main/java/com/example/day2part2/controller/Home.java
@@ -1,12 +1,14 @@
 package com.example.day2part2.controller;
 
+import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
-    @GetMapping("about/")
-    public String about() {
-        return "New Hello";
+    @GetMapping("/")
+    public StudentModel getStudent() {
+        StudentModel student = new StudentModel(1, "Prakhar", "ps@gmail.com");
+        return student;
     }
 }
